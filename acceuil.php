6d5ced4a7d1f88bf3b20d5c727a3cee30b290144
Vincent <borgel.vincent@gmail.com>
2019-01-16 15:21:32 +0100

    second commit

---
+++ acceuil.php
@@ -0,0 +1,104 @@
+<div style="width: 200px; height: 45px;  background-color:white; border-radius: 10px; top:-100px; left:50px; position: relative; "><center> <a href="contact.php" type="button" ><h2>Contact<h2></a></center>
+</div>
+
+<div class="container">
+    <div class="row">
+
+        <!-- carré Réseau-->
+        <div class="col-sm-4 pt-3" >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/reseauinformatique.png" alt="réseau" style="height:250px" >
+                <div class="card-body">
+                    <p class="card-text"><br><br>Votre ordinateur (que l'on appellera le client) va chercher à entrer en communication avec un autre ordinateur (que l'on appellera le serveur) ...<a href="filereseau.html">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré HTML5-->
+        <div class="col-sm-4 pt-3" >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/HTML5.png" alt="HTML 5" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">HTML5 (HyperText Markup Language 5) est la dernière révision majeure du HTML (format de données conçu pour représenter les pages web)...
+                        <a href="html5.php">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré CSS3-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/CSS3_logo.png" alt="logo CSS" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets...<a href="css3.php">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré SQL-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/SQL.jpeg" alt="CSS" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">SQL (sigle de Structured Query Language, en français langage de requête structurée) est un langage informatique normalisé servant à exploiter des bases de données relationnelles...
+                     <a href= "filesql.html">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré Java-Sript-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/JS.png" alt="Java Script" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs avec l'utilisation...
+                    <a href= "filejavascript.html">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré JAVA-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/Java.png" alt="Card image cap" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">Java est un langage de programmation orienté objet créé par James Gosling et Patrick Naughton, employés de Sun Microsystems, avec le soutien de Bill Joy...
+                    <a href="java.php">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré PYTHON-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/python-logo.jpg" alt="Card image cap" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">Python est un langage de programmation objet interprété, multi-paradigme et multiplateformes. Il favorise la programmation impérative structurée, fonctionnelle et orientée objet...
+                    <a href="filephyton.html">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré RUBY-->
+        <div class="col-sm-4 pt-3" >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/ruby.png" alt="logo ruby" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text">Ruby est un langage de programmation libre. Il est interprété, orienté objet et multi-paradigme...
+                        <a href="fileruby.html">en savoir plus</a> </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- carré PHP-->
+        <div class="col-sm-4 pt-3"  >
+            <div class="card" style="height:500px">
+                <img class="card-img-top" src="img/php.png" alt="Card image cap" style="height:250px">
+                <div class="card-body">
+                    <p class="card-text"><br><br>PHP: Hypertext Preprocessor, plus connu sous son sigle PHP (acronyme récursif), est un langage de programmation libre, principalement utilisé pour produire des pages Web dynamiques via un serveur HTTP...
+                    <a href="filephp.html">en savoir plus</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
